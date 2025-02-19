e57e1876581b7c4d28feedda1115b6cae995a5b7
yeoni-rabbit <soyeon4818@gmail.com>
2025-02-19 19:52:50 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import '../lib/smooth';
+import './styles/style.css';
+
+
+
+// import { gsap } from "gsap";
+// import { Draggable } from "gsap/Draggable";
+// import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// gsap.registerPlugin(ScrollTrigger,Draggable,MotionPathPlugin);
+
+
+
+
+
+// 1. 터미널 창에서 npm i 실행한다.
+// 2. 깃헙에 올린다.
+// 3. npm run dev로 사이트를 연다.
+// 4. 보안 설정 다 수락하기
+// 5. 코딩 시작.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
