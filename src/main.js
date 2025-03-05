ca1518e29c58ee2f2e43e3c84a1d0928be6ed334
kindtiger <seonbeom2@gmail.com>
2025-03-05 20:35:10 +0900

    4차 컨펌

--- src/main.js
+++ src/main.js
@@ -1,24 +1,13 @@
-import '../lib/smooth';
-import './styles/style.css';
+import "../lib/smooth";
+import "./styles/style.css";
+import { markers } from "../lib/smooth";
 
-
-
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 // import { Draggable } from "gsap/Draggable";
 // import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// gsap.registerPlugin(ScrollTrigger,Draggable,MotionPathPlugin);
-
-
-
-
-
-// 1. 터미널 창에서 npm i 실행한다.
-// 2. 깃헙에 올린다.
-// 3. npm run dev로 사이트를 연다.
-// 4. 보안 설정 다 수락하기
-// 5. 코딩 시작.
+gsap.registerPlugin(ScrollTrigger);
 
 
 
@@ -26,6 +15,34 @@ import './styles/style.css';
 
 
 
+ScrollTrigger.create({
+  trigger: '.section02',
+  start: 'top 20%',
+  // end: 'bottom bottom',
+  // animation: gsap.to('.overDeco_circle img',{duration:1,stagger:0.2,scale:1,ease:"back.inOut(1.7)"}),
+  animation: gsap.from('.overDeco_eyes',{y:300,opacity:0}),
+  toggleActions: 'restart reverse restart reverse',
+  // pin: false,
+  // pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+})
+
+
+
+
+ScrollTrigger.create({
+  trigger: '.section03',
+  start: 'top center',
+  end: '50% bottom',
+  // animation: gsap.to('.overDeco_circle img',{duration:1,stagger:0.2,scale:1,ease:"back.inOut(1.7)"}),
+  animation: gsap.from('.section03 .titleGroup > div',{stagger:0.1,y:30,opacity:0}),
+  // toggleActions: 'restart reverse restart reverse',
+  // pin: false,
+  // pinSpacing: false,
+  // markers: true,
+  scrub: true,
+})
 
 
 
@@ -38,6 +55,17 @@ import './styles/style.css';
 
 
 
+
+
+
+
+
+
+
+
+
+
+markers()
 
 
 
